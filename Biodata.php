fb2a37436a73a3a15a36ba21d6d8c5de5bc5e98e
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2021-09-15 14:24:14 +0700

    Create Biodata.php

---
+++ Biodata.php
@@ -0,0 +1,21 @@
+<?php
+    $nama   = 'Ujun Junaedi';
+    $kelas  = 'MI20B';
+    $email  = 'jujunjuna189@gmail.com';
+?>
+<html>
+<head>
+    <title>Biodata Ujun Junaedi</title>
+</head>
+<body>
+<?php
+    echo 'BIODATA';
+    echo '<br><br>';
+    echo 'My Name Is ' . '<b>' . $nama . '</b>';
+    echo '<br>';
+    echo 'My Class ' . '<b>' . $kelas . '</b>';
+    echo '<br>';
+    echo 'My Email Address ' . '<b>' . $email . '</b>';
+?>
+</body>
+</html>
