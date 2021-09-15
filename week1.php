96953a71f41d040299f21073bce9456416cccb82
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2021-09-15 14:24:31 +0700

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,17 @@
+<?php
+    $nama = "Ujun Junaedi";
+?>
+<!DOCTYPE html>
+<head>
+    <title>First Week - Ujun Junaedi</title>
+</head>
+<body>
+<?php
+    echo 'Hello World';
+    echo '<br>'; //Berfungsi untuk membuat garis baru
+    echo $nama;
+    echo '<br>';
+    echo '<b>Hello</b> ' . $nama;
+?>
+</body>
+</html>
