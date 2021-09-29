d8ee4a21c8f5573e934c82f2b647c0fdd75acd3f
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2021-09-29 14:09:51 +0700

    Create Connection.php

---
+++ Connection.php
@@ -0,0 +1,37 @@
+<?php
+$username = 'root';
+$password = '';
+$database = 'db_surat';
+
+$con = new mysqli('localhost', $username, $password, $database);
+
+if ($con->connect_error) {
+    die('Connection Failed');
+}
+
+$sql = "SELECT * FROM tbl_surat WHERE id='2'";
+$result = $con->query($sql);
+$data = $result->fetch_assoc();
+
+$js = '';
+
+if ($data['jenis_surat'] == 1) {
+    $js = 'SURAT KEPUTUSAN';
+} else if ($data['jenis_surat'] == 2) {
+    $js = 'SURAT PERNYATAAN';
+} else if ($data['jenis_surat'] == 3) {
+    $js = 'SURAT PEMINJAMAN';
+} else {
+    die('Jenis Surat Tidak Terdaftar');
+}
+
+$surat = array(
+    'date'          => $data['tgl_surat'],
+    'nomor'         => $data['no_surat'],
+    'kepada'        => $data['ttd_surat'],
+    'kota'          => 'Tasikmalaya',
+    'instansi'      => array('LP3I, ', 'Kota Tasikmalaya, ', '081297551925'),
+    'barang'        => array('Kamera', 'Komputer'),
+    'ttd'           => $data['ttd_menyetujui'],
+    'js'            => $js,
+);
